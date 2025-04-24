8517a0dea601fb3be040aa3f414e9521a1ee98c6
Krishan <krishansapkota@gmail.com>
2025-04-23 20:08:42 -0400

    Initial Commit

---
+++ src/main/java/com/example/quoteapi/HelloController.java
@@ -0,0 +1,24 @@
+//all files in this package for application
+package com.example.quoteapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+//Spring annotation; Rest Controller tells spring this class handles
+// web requests, and will return JSOn or text file, not HTML page
+@RestController  //Spring annotation, tells spring
+public class HelloController {
+
+    //spring annotation, get request to /hello
+    @GetMapping("/hello")
+    //map/string returns JSOn style key value pair
+    public Map<String, String> hello() {
+        return Map.of("message", "Hello World");
+    }
+
+    //@PostMapping("/hello")
+
+
+}
---
+++ src/main/java/com/example/quoteapi/QuoteapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.quoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteapiApplication.class, args);
+	}
+
+}
